dfd29045d480068dd96ef7f68eeb22e594baa79c
Viktoriia Vynohradova <viktoriia.vynohradova8@gmail.com>
2024-01-18 19:34:21 +0200

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,39 @@
+"use strict";
+const list = document.querySelectorAll("li");
+const ul = document.querySelector("ul");
+function convertToNumber(text) {
+    return +text.replace("$", "").replace(",", "");
+}
+const sortList = (input)=>{
+    return [
+        ...input
+    ].sort((item1, item2)=>{
+        if (convertToNumber(item1.dataset.salary) === convertToNumber(item2.dataset.salary)) return item1.innerText.localeCompare(item2.innerText);
+        return convertToNumber(item1.dataset.salary) < convertToNumber(item2.dataset.salary) ? 1 : -1;
+    });
+};
+const sortedList = sortList(list);
+ul.innerHTML = `
+  ${sortedList.map((item)=>`
+    <li
+      data-position={item.dataset.position}
+      data-salary={item.dataset.salary}
+      data-age={item.dataset.age}
+    >
+      ${item.innerText}
+    </li>
+  `).join("")}
+`;
+const getEmployees = (input)=>{
+    return [
+        ...input
+    ].map((item)=>({
+            name: item.innerText,
+            position: item.dataset.position,
+            salary: item.dataset.salary,
+            age: item.dataset.age
+        }));
+};
+getEmployees(list);
+
+//# sourceMappingURL=index.f75de5e1.js.map
